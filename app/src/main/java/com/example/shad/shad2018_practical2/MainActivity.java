1762953e9c16a23699246e2ed57403a29bbec974
Stanislav Kulikov <satsakul@yandex-team.ru>
2018-01-26 03:15:52 +0300

    relative_layout

--- app/src/main/java/com/example/shad/shad2018_practical2/MainActivity.java
+++ app/src/main/java/com/example/shad/shad2018_practical2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.shad.shad2018_practical2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -9,10 +13,26 @@ import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner mAges;
+    private TextView mName;
+    private Spinner mSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.relative_layout);
+
+        mName = findViewById(R.id.name);
+        mSpinner = findViewById(R.id.cities);
+        mAges = findViewById(R.id.ages);
+    }
+
+    public void clickButton(View view) {
+        final String age = String.valueOf(mAges.getSelectedItem());
+        final String city = String.valueOf(mSpinner.getSelectedItem());
+        final String name = String.valueOf(mName.getText());
+
+        Toast.makeText(this, "Name = " + name + ", city = " + city + ", age = " + age, Toast.LENGTH_SHORT).show();
     }
 }
